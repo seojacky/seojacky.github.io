0818b4a95352b90996d82d6aacc5e781e9ba70f1
Eugen <kalinsky30@gmail.com>
2025-08-22 00:07:21 +0300

    Create config.js

---
+++ pwa/settings/universities/kntu/config.js
@@ -0,0 +1,62 @@
+// config.js - Конфігурація розкладу дзвінків ХНТУ
+export default {
+  "university": {
+    "code": "kntu",
+    "name": "Херсонський національний технічний університет",
+    "shortName": "ХНТУ",
+    "timeZone": "Europe/Kiev"
+  },
+  "schedule": {
+    "bellSchedule": [
+      {
+        "number": 1,
+        "start": "08:30",
+        "end": "09:50",
+        "duration": 80,
+        "breakAfter": 15
+      },
+      {
+        "number": 2,
+        "start": "10:05",
+        "end": "11:25",
+        "duration": 80,
+        "breakAfter": 15
+      },
+      {
+        "number": 3,
+        "start": "11:40",
+        "end": "13:00",
+        "duration": 80,
+        "breakAfter": 15
+      },
+      {
+        "number": 4,
+        "start": "13:15",
+        "end": "14:35",
+        "duration": 80,
+        "breakAfter": 15
+      },
+      {
+        "number": 5,
+        "start": "14:50",
+        "end": "16:10",
+        "duration": 80,
+        "breakAfter": 15
+      },
+      {
+        "number": 6,
+        "start": "16:25",
+        "end": "17:45",
+        "duration": 80,
+        "breakAfter": 15
+      },
+      {
+        "number": 7,
+        "start": "18:00",
+        "end": "19:20",
+        "duration": 80,
+        "breakAfter": 0
+      }
+    ]
+  }
+};
